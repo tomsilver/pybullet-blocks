ee72e03ced396a57d43da6a02fbf8701db3d690d
Tom Silver <tomssilver@gmail.com>
2024-10-05 13:06:27 -0400

    wip

--- tests/envs/test_pick_place_env.py
+++ tests/envs/test_pick_place_env.py
@@ -1,8 +1,8 @@
 """Tests for pick_place_env.py."""
 
 from pybullet_blocks.envs.pick_place_env import PickPlacePyBulletBlocksEnv, PickPlacePyBulletBlocksState
-from pybullet_helpers.motion_planning import run_smooth_motion_planning_to_pose
-from pybullet_helpers.geometry import Pose
+from pybullet_helpers.motion_planning import run_smooth_motion_planning_to_pose, smoothly_follow_end_effector_path, create_joint_distance_fn
+from pybullet_helpers.geometry import Pose, interpolate_poses
 import numpy as np
 from gymnasium.wrappers import RecordVideo
 
@@ -49,4 +49,24 @@ def test_pick_place_env():
         obs, _, _, _, _ = env.step(action)
         state = PickPlacePyBulletBlocksState.from_vec(obs)
 
+    # Move down to grasp the block.
+    sim.set_state(obs)
+    end_effector_path = list(interpolate_poses(
+        sim.robot.get_end_effector_pose(),
+        Pose(state.block_pose.position, robot_grasp_orientation),
+        include_start=False,
+    ))
+    joint_distance_fn = create_joint_distance_fn(sim.robot)
+    plan = smoothly_follow_end_effector_path(sim.robot, end_effector_path,
+                                      state.robot_joints, sim.get_collision_ids(),
+                                      joint_distance_fn,
+                                      max_time=max_motion_planning_time,
+                                      include_start=False)
+    assert plan is not None
+    for joint_state in plan:
+        action = np.subtract(joint_state, state.robot_joints).astype(np.float32)
+        assert env.action_space.contains(action)
+        obs, _, _, _, _ = env.step(action)
+        state = PickPlacePyBulletBlocksState.from_vec(obs)
+    
     env.close()
