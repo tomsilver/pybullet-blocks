4a6afeb342d3c1e4723f14c1f9d32566547ef43d
SkywalkerJLi <126359826+SkywalkerJLi@users.noreply.github.com>
2025-06-11 16:53:10 -0400

    Add stacking field to LabeledObjectState (#49)
    * Add stacking parameter to BlockStackingEnv
    
    * addressed comments
    
    ---------
    
    Co-authored-by: Skywalker Li <skywalkerli@MacBookPro.T-mobile.com>

--- tests/envs/test_block_stacking_env.py
+++ tests/envs/test_block_stacking_env.py
@@ -11,4 +11,8 @@ def test_block_stacking_env():
     env = BlockStackingPyBulletObjectsEnv(use_gui=False)
     obs, _ = env.reset(seed=123, options={"init_piles": [["I", "H"], ["M", "O", "T"]]})
     assert len(obs.nodes) == 6
+    print(obs.nodes)
+    assert len(obs.edge_links) == 3
+    assert obs.edge_links[0][0] == 7
+    assert obs.edge_links[0][1] == 8
     env.close()
